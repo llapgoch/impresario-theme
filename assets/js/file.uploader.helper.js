5ef5e51e5e122c386a61887ed6d640f7be79830d
Dave Baker <dave_preece@mac.com>
2019-01-09 12:11:28 +0000

    Made block replacer more concise

--- assets/js/file.uploader.helper.js
+++ assets/js/file.uploader.helper.js
@@ -8,16 +8,11 @@
             deleteSelector = '.js-delete-confirm';
 
         function initialise(){
-            /*  This is nasty, but is this way because: tableupdatersucces fires before 
-                the ajaxSuccess global method which replaces the HTML and reinits the widget
-                (therefore the complete event won't fire on the element). Set the interval so 
-                that the HTML has been replaced and we can initialise on the new element */
             if($.fn.itemDeleter){
-                $(uploadTableSelector).on('tableupdatersuccess', function(){
-                    window.setTimeout(function(){
-                        jQuery(deleteSelector).itemDeleter();
-                        initialise();
-                    }, 10);
+                $(uploadTableSelector).on('block-replacer-before', function(ev, params){
+                    if(params['newBlock']){
+                        $(deleteSelector ,params['newBlock']).itemDeleter();
+                    }
                 });
             }
         }
