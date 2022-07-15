be9469dcdcd72a5c1e48a6f6dece69f6b8adfe86
Dave Baker <dave@awaredigital.co.uk>
2022-07-15 16:00:34 +0100

    Project calculations in JS'

--- assets/js/project/edit-controller.js
+++ assets/js/project/edit-controller.js
@@ -1,7 +1,13 @@
 ; (function ($) {
     $(document).on('ready', function () {
         var rebateControlSelector = '.js-rebate-control',
-            rebatePercentageSelector = '.js-rebate-percentage'
+            rebatePercentageSelector = '.js-rebate-percentage',
+            rebateAmountSelector = '.js-rebate-amount',
+            netSellSelector = '.js-net-sell',
+            actualCostSelector = '.js-actual-cost',
+            actualProfitSelector = '.js-actual-profit ',
+            actualMarginSelector = '.js-actual-margin';
+
 
 
         addEvents();
@@ -12,6 +18,65 @@
             $document.on('change', rebateControlSelector, function (ev) {
                 updateRebateControl();
             });
+
+            $(rebatePercentageSelector).on('keyup', function(ev){
+                calculateRebate();
+            });
+
+        }
+
+        function calculateRebate() {
+            var netSell = parseFloat($(netSellSelector).data('actual-value'));
+            var $rebateEl = $(rebatePercentageSelector);
+            var $actualCostEl = $(actualCostSelector);
+            var $rebateAmountEl = $(rebateAmountSelector);
+            var $actualProfitEl = $(actualProfitSelector);
+            var $actualMarginEl = $(actualMarginSelector);
+            var costItemTotal = parseFloat($actualCostEl.data('item-total'));
+
+            var rebateOriginalValue = $rebateEl.val();
+            var rebateOriginalValue, rebateValue = parseFloat(rebateOriginalValue);
+
+
+            if(isNaN(netSell)) {
+                netSell = 0;
+            }
+
+            if(isNaN(rebateValue)) {
+                rebateValue = 0;
+            }
+
+            if(isNaN(costItemTotal)) {
+                costItemTotal = 0;
+            }
+
+            rebateValue = Math.max(0, Math.min(rebateValue, 100));
+
+            // Clamp & reset the rebate percentage, if entered
+            if(rebateValue !== rebateOriginalValue && rebateOriginalValue !== '') {
+                $rebateEl.val(rebateValue);
+            }
+
+            var rebateAmount = netSell * (rebateValue / 100);
+            // Round up - 2 decimal places
+            rebateAmount = Math.ceil(rebateAmount * 100) / 100;
+            var totalActualCost = (rebateAmount + costItemTotal);
+
+            // Calculate actual profit
+            var actualProfit = netSell - totalActualCost;
+            var actualMargin = 0;
+            
+            
+            if(netSell > 0) {
+                actualMargin = (actualProfit / netSell) * 100;
+                actualMargin = Math.round(actualMargin * 100) / 100;
+            }
+
+            $rebateAmountEl.val("£" + (rebateAmount.toFixed(2)));
+            $actualCostEl.val("£" + (totalActualCost.toFixed(2)));
+            $actualProfitEl.val("£" + (actualProfit.toFixed(2)));
+            $actualMarginEl.val(actualMargin + "%");
+            
         }
 
         function updateRebateControl() {
@@ -24,6 +89,8 @@
                     .val('0.00');
 
             }
+
+            calculateRebate();
         }
     });
 }(jQuery));
